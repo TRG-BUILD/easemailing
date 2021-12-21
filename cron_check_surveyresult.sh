3f6c91b7bac2f0608defb24f38b518a93420a469
pellegoeg <pellegoeg@users.noreply.github.com>
2021-12-21 09:37:41 +0100

    removed directories from cron scripts

--- cron_check_surveyresult.sh
+++ cron_check_surveyresult.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 . $HOME/miniconda3/bin/activate base
-cd /srv/easemailing
 python check_surveyresult.py -c jobcfg.json >> survey_log2.log
--- cron_job.sh
+++ cron_job.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 . $HOME/miniconda3/bin/activate base
-cd /srv/easemailing
 python job.py -c jobcfg.json >> survey_log.log
