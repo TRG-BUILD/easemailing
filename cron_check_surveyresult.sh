96c024269beac2c839f138d5f173ea8b81450207
pellegoeg <pellegoeg@users.noreply.github.com>
2021-12-21 09:55:53 +0100

    fixed working directory in shell scripts

--- cron_check_surveyresult.sh
+++ cron_check_surveyresult.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd "$(dirname "$(realpath "$0")")";
 . $HOME/miniconda3/bin/activate base
 python check_surveyresult.py -c jobcfg.json >> survey_log2.log
--- cron_job.sh
+++ cron_job.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd "$(dirname "$(realpath "$0")")";
 . $HOME/miniconda3/bin/activate base
 python job.py -c jobcfg.json >> survey_log.log
